9ee58da3944d2b1b33ae1d640309e3eea53cb600
Wycliff Susiku mubita <wycliffsusikumubita@pop-os.localdomain>
2022-10-09 21:19:06 +0200

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+  "./public/*.html"
+  ],
+  theme: {
+    screens:{
+      "sm":"375px",
+      "xl":"1440px"
+    },
+    extend: {
+      colors:{
+        "Bright-Orange":"hsl(31, 77%, 52%)",
+        "Dark-Cyan":"hsl(184, 100%, 22%)",
+        "Black-Cyan":"hsl(179, 100%, 13%)",
+        "Transparent-White":" hsla(0, 0%, 100%, 0.75)",
+        "Light-Gray":"hsl(0, 0%, 95%)"
+      }
+    }
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
+}
